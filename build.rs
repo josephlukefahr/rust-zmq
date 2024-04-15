13ffb18b120ee350e36172c6d69db2fa49f68d8d
Joseph Lukefahr <joseph.lukefahr@nps.edu>
2024-04-14 21:51:37 -0700

    small tweak to build script

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() {
         "cargo:rustc-env=BUILD_PROFILE={}",
         std::env::var("PROFILE").unwrap()
     );
-    println!("cargo:rerun-if-changed=build/main.rs");
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=PROFILE");
     println!("cargo:rustc-link-lib=zmq");
 }
